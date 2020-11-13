1a32b3fa16d7feab6bdbf2fe6bdf8438a71960a0
Rich Layte <rich.layte@gmail.com>
2020-11-13 16:54:07 +0000

    Service tf module init on create

--- cmd/service_create.go
+++ cmd/service_create.go
@@ -6,18 +6,58 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"regexp"
 	"strings"
 	"text/template"
 
+	"github.com/iancoleman/strcase"
 	"github.com/spf13/cobra"
 )
 
 var serviceTemplate string
+var validServiceName = regexp.MustCompile(`^[a-zA-Z\-]+$`).MatchString
+
+var terraformModuleInit = `
+module "service-{{.ServiceName}}" {
+	source                  = "../services/{{.ServiceName}}"
+	container_project       = var.container_project
+	environment             = var.environment
+	project_id              = google_project.project.project_id
+	project_number          = google_project.project.number
+	region                  = var.region
+	private_network         = google_compute_network.private_network.self_link
+	service_depends_on      = [google_service_networking_connection.private_vpc_connection, google_project_iam_member.container_access, google_project_iam_binding.cloudsql]
+}
+`
 
 type serviceTemplateVars struct {
 	ServiceName string
 	ProjectDir  string
 	ServicesDir string
+}
+
+func initServiceTerraform(name string) error {
+	projectsConfig, err := readProjectsConfig()
+	if err != nil {
+		return err
+	}
+	projectDir := projectsConfig.Projects[projectsConfig.Selected]
+	serviceModulesPath := path.Join(projectDir, "infra", "services.tf")
+	fmt.Println("Adding service to modules:", serviceModulesPath)
+
+	f, err := os.OpenFile(serviceModulesPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	t, err := template.New("service-terraform-init").Parse(terraformModuleInit)
+	if err != nil {
+		return err
+	}
+	if t.Execute(f, &serviceTemplateVars{ServiceName: name}); err != nil {
+		return err
+	}
+	return nil
 }
 
 func createService(name string) error {
@@ -50,6 +90,9 @@ func createService(name string) error {
 		"ToUpper": strings.ToUpper,
 		"ToLower": strings.ToLower,
 		"Title":   strings.Title,
+		"ToCamel": strcase.ToCamel,
+		"ToKebab": strcase.ToKebab,
+		"ToSnake": strcase.ToSnake,
 	}
 
 	isRoot := true
@@ -106,6 +149,11 @@ var createServiceCmd = &cobra.Command{
 		if len(args) < 1 {
 			return errors.New("No service name provided")
 		}
+
+		name := args[0]
+		if validServiceName(name) != true {
+			return errors.New("Service names may only contain letters, dashes, and underscores")
+		}
 		return nil
 	},
 	Run: func(cmd *cobra.Command, args []string) {
@@ -113,6 +161,15 @@ var createServiceCmd = &cobra.Command{
 
 		if err := createService(name); err != nil {
 			fmt.Printf("Error creating service: %v\n", err)
+			os.Exit(1)
+		}
+		if err := initServiceTerraform(name); err != nil {
+			fmt.Printf("Error generating proto: %v\n", err)
+			os.Exit(1)
+		}
+		servicePath := path.Join("services", name)
+		if err := genProto(servicePath); err != nil {
+			fmt.Printf("Error generating proto: %v\n", err)
 			os.Exit(1)
 		}
 	},
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/go-git/go-git/v5 v5.2.0
+	github.com/iancoleman/strcase v0.1.2
 	github.com/spf13/cobra v1.0.0
 	gopkg.in/yaml.v2 v2.3.0
 )
